3f1ed0215450ac735e6643be095bd98fe4e34087
omkar-mahajan <ommahajan170@gmail.com>
2022-07-12 16:18:00 +0530

    Changes by b

--- GitDemo/src/demo/DemoGit.java
+++ GitDemo/src/demo/DemoGit.java
@@ -7,10 +7,19 @@ public class DemoGit {
 
 	}
 
+	public int sub2(int a, int b) {
+		return a - b;
+
+	}
+	
+	
 	public static void main(String[] args) {
 		DemoGit d = new DemoGit();
 
 		System.out.println(d.sub(50, 40));
+		DemoGit d2 = new DemoGit();
+
+		System.out.println(d2.sub2(50, 40));
 
 	}
 
